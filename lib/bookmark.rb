52e065390dae4ca539c999e3272b89c8513eee47
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-01-28 15:59:16 +0000

    shows a list of the bookmarks

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,9 +2,6 @@ class Bookmark
 
   attr_reader :url
 
-  def initialize(url)
-    @url = url
-  end
 
   def self.all
     [
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,10 +6,6 @@ describe Bookmark do
       'http://www.google.com'
     ]
   end
-  it 'has a link' do
-    buzzfeed = Bookmark.new('wwww.buzzfeed.com')
-    expect(buzzfeed.url).to eq 'wwww.buzzfeed.com'
-  end
 
   it 'shows a list of the bookmarks' do
     expect(Bookmark.all).to eq(bookmarks)
